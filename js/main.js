3dedab74ec202bbd85cd2cc151d64621aa7058e2
Sabohatfrontend <ssobirova333@gmail.com>
2022-01-24 22:33:24 +0500

    Correct bacground img

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const elbutton = document.querySelector('.button-add');
+elbutton.addEventListener('click', function(){
+elbutton.classList.toggle('button-add-active');
+console.log("ishladi");
+});
